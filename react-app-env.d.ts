81c5a0a8c0def7bc2a6895a0d77d981b8a57d7b1
Aheto <30739905+Aheto@users.noreply.github.com>
2025-12-23 23:33:19 +0000

    Add files via upload

---
+++ react-app-env.d.ts
@@ -0,0 +1,24 @@
+// C:\Users\JNR\mini-openstax\frontend\src\react-app-env.d.ts
+
+/// <reference types="react-scripts" />
+
+// Declare modules for static assets
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
